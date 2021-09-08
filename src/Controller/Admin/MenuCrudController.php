6af1ea9e0e82dff8a46ad51f477a3803ae881ee9
Toohk <thomasdufayet3@gmail.com>
2021-09-08 11:18:46 +0200

    fix menu

--- src/Controller/Admin/MenuCrudController.php
+++ src/Controller/Admin/MenuCrudController.php
@@ -4,6 +4,12 @@ namespace App\Controller\Admin;
 
 use App\Entity\Menu;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Vich\UploaderBundle\Form\Type\VichImageType;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
 
 class MenuCrudController extends AbstractCrudController
 {
@@ -12,14 +18,14 @@ class MenuCrudController extends AbstractCrudController
         return Menu::class;
     }
 
-    /*
+   
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
+            TextField::new('name'),
             TextEditorField::new('description'),
+            TextField::new('price'),
         ];
     }
-    */
+   
 }
